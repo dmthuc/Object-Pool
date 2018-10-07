7e27e66cff04c6fecc414331a8cfe5a459b2f487
Dao Minh Thuc <daominhthuc@Daos-MacBook-Pro.local>
2018-10-07 22:34:20 +0700

    More constructors and support range for(without proper test)

--- pool.hpp
+++ pool.hpp
@@ -18,15 +18,38 @@
 #include <array>
 #include <memory>
 #include <bitset>
+#include <stdexcept>
 
 template <class T, size_t N> 
 class Pool {
 public:
+    using const_iterator = typename std::array<T,N>::const_iterator;
+
     template<class ...Args>
     Pool(Args&& ...args)
         :num_of_avail_(N), storage_{}, status_{}
     {
         storage_.fill(T{std::forward<Args>(args)...});
+        status_.set();
+    }
+
+    template<class Iterator>
+    Pool(Iterator begin, Iterator end)
+        :num_of_avail_(N), storage_{}, status_{}
+    {
+        if (N != std::distance(begin, end))
+            throw std::runtime_error("distance between begin and end isn't:" + std::to_string(N));
+        
+        std::move(begin, end, storage_.begin()); 
+        status_.set();
+    }
+
+    Pool(std::initializer_list<T> init)
+        :num_of_avail_{N}, storage_{}, status_{}
+    {
+        if (init.size() != N)
+            throw std::runtime_error("mismatch size of initializer_list and pool"); 
+        std::copy(init.begin(), init.end(), storage_.begin()); 
         status_.set();
     }
 
@@ -56,6 +79,16 @@ public:
     }
 
     size_t num_of_avail() const noexcept {return num_of_avail_;};
+    const_iterator begin() const
+    {
+        return storage_.cbegin();
+    }
+
+    
+    const_iterator end() const
+    {
+        return storage_.cend();
+    }
 private:
     size_t num_of_avail_;
     std::array<T,N> storage_; 
--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cassert>
 #include <string>
+#include <vector>
 #define private public
 #include "pool.hpp"
 
@@ -10,17 +11,49 @@ using namespace std;
 struct Foo {
     Foo(int _a, string _b)
         :a(_a), b(std::move(_b))
-    {}
+    {
+        cout<<"constructor\n";
+    }
+
     Foo()
         :a{},b{}
-    {}
+    {
+        cout<<"default constructor\n";
+    }
+    Foo(const Foo& f)
+        :a{f.a}, b{f.b}
+    {
+        cout<<"copy constructor\n";
+    }
+    Foo(Foo&& f)
+        :a{f.a}, b{std::move(f.b)}
+    {
+        cout<<"move constructor\n"; 
+    }
+    
+    Foo& operator=(const Foo& other)
+    {
+        a= other.a;
+        b= other.b;
+        cout<<"copy assignment";
+        return *this;
+    }
+
+    Foo& operator=(Foo&& other)
+    {
+        a=other.a;
+        b=std::move(other.b);
+        cout<<"move assignment\n";
+        return *this;
+    }
+    
     int a;
     string b;
 };
 
 void test_with_compound_type()
 {
-    Pool<Foo, 6> pool{1, string{"some string"}};
+    Pool<Foo, 6> pool(1, string{"some string"});
     auto obj1 = pool.acquire();
     assert(!pool.status_[0]);
     if (nullptr != obj1.get())
@@ -65,10 +98,21 @@ void test_with_integral_type()
         cout<<"no obj available\n";
 }
 
+void constructor_test()
+{
+    vector<Foo> v{Foo{1,"a"}, Foo{2,"b"}};     
+    Pool<Foo, 2> pool{v.begin(), v.end()}; 
+    Pool<Foo, 2> pool2{Foo{1,"a"}, Foo{2,"b"}};
+    for( const auto& e: pool2)
+        cout<<e.b<<endl;
+}
+
+
 int main()
 {
     test_with_integral_type();
     test_with_compound_type();
+    constructor_test();
     cout<<"All test passed"<<endl;
 }
 
